21e117641038571fa93c78acc9c9a88153d75876
Jamie Briggs <jamie.briggs@hotmail.co.uk>
2019-04-05 23:32:56 +0100

    Made start on POS page.

---
+++ Site/Controllers/POSController.cs
@@ -0,0 +1,20 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class POSController : Controller
+    {
+        // GET: POS
+        public ActionResult Index()
+        {
+            DatabaseController db = new DatabaseController();
+            var items = db.GetItems();
+            return View(items);
+        }
+    }
+}
