defd3eaaf2de15f0c7adbc335930362052c07e52
Jamie Briggs <personal@jbriggs.dev>
2019-04-19 03:07:46 +0100

    Added checks through js to create views.

--- Database/Model/Item.cs
+++ Database/Model/Item.cs
@@ -14,12 +14,16 @@ namespace Database.Model
         [Key]
         public int ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
+        [Required]
         public int Quantity { get; set; }
 
+        [Required]
         public Type Component { get; set; }
 
         public virtual List<Item> Items { get; set; }
